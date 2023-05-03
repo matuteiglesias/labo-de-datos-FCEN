a2745ebb6a6d76939c15954bf4728d35b260ed84
matuteiglesias <>
2023-05-02 23:00:02 -0300

    Automatic commit

--- ejercicios/formatted.py
+++ ejercicios/formatted.py
@@ -1,44 +1,68 @@
 # %% [markdown]
 # ## Introducción a los datos no estructurados
 
+
+
 # %% [markdown]
 # ### 1. Cargar un archivo de texto y contar la cantidad de palabras.
 
+
+
 # %% [markdown]
 # #### a. Importar la librería os y utilizarla para obtener la ruta del archivo:
+
+
 
 # %%
 import os
 
 ruta = os.path.join('ruta', 'del', 'archivo', 'texto.txt')
 
+
+
 # %% [markdown]
 # #### b. Abrir el archivo y leer su contenido:
+
+
 
 # %%
 with open(ruta, 'r', encoding='utf-8') as f:
     contenido = f.read()
 
+
+
 # %% [markdown]
 # #### c. Tokenizar el contenido del archivo en palabras:
+
+
 
 # %%
 import nltk
 
 palabras = nltk.word_tokenize(contenido)
 
+
+
 # %% [markdown]
 # #### d. Contar la cantidad de palabras:
+
+
 
 # %%
 len(palabras)
 
 
+
+
 # %% [markdown]
 # ### 2. Cargar una imagen y mostrar su contenido.
 
+
+
 # %% [markdown]
 # #### a. Importar la librería Pillow y utilizarla para cargar la imagen:
+
+
 
 # %%
 from PIL import Image
@@ -47,54 +71,86 @@ ruta = os.path.join('ruta', 'del', 'archivo', 'imagen.jpg')
 
 imagen = Image.open(ruta)
 
+
+
 # %% [markdown]
 # #### b. Mostrar la imagen en una ventana emergente:
+
+
 
 # %%
 imagen.show()
 
 
 
+
+
 # %% [markdown]
 # ### 1. Leer y visualizar una imagen utilizando Python.
 
+
+
 # %% [markdown]
 # #### a. Importar las bibliotecas necesarias:
+
+
 
 # %%
 import matplotlib.pyplot as plt
 import cv2
 
+
+
 # %% [markdown]
 # #### b. Leer la imagen utilizando la función imread de OpenCV:
+
+
 
 # %%
 img = cv2.imread('ruta_de_la_imagen.jpg')
 
+
+
 # %% [markdown]
 # #### c. Visualizar la imagen utilizando la función imshow de Matplotlib:
+
+
 
 # %%
 plt.imshow(img)
 
+
+
 # %% [markdown]
 # ### 2. Leer y procesar un archivo de texto utilizando Python.
 
+
+
 # %% [markdown]
 # #### a. Leer el archivo de texto utilizando la función open de Python:
+
+
 
 # %%
 with open('ruta_del_archivo_de_texto.txt', 'r') as f:
     data = f.read()
 
+
+
 # %% [markdown]
 # #### b. Tokenizar el texto utilizando la función split de Python:
+
+
 
 # %%
 tokens = data.split()
 
+
+
 # %% [markdown]
 # #### c. Calcular la frecuencia de cada token utilizando un diccionario de Python:
+
+
 
 # %%
 freq = {}
@@ -104,8 +160,12 @@ for token in tokens:
     else:
         freq[token] = 1
 
+
+
 # %% [markdown]
 # #### d. Ordenar los tokens según su frecuencia en orden descendente:
+
+
 
 # %%
 sorted_freq = {k: v for k, v in sorted(freq.items(), key=lambda item: item[1], reverse=True)}
@@ -113,19 +173,29 @@ print(sorted_freq)
 
 
 
+
+
 # %% [markdown]
 # ### 2. Utilizar la librería spaCy para procesar un texto y obtener información relevante.
 
+
+
 # %% [markdown]
 # #### a. Importar spaCy y cargar el modelo de lenguaje en español:
+
+
 
 # %%
 import spacy
 
 nlp = spacy.load('es_core_news_sm')
 
+
+
 # %% [markdown]
 # #### b. Procesar un texto y obtener información relevante, como entidades nombradas y sus tipos:
+
+
 
 # %%
 text = "El presidente Alberto Fernández anunció un paquete de medidas económicas para impulsar el crecimiento del país."
@@ -136,8 +206,12 @@ doc = nlp(text)
 for ent in doc.ents:
     print(ent.text, ent.label_)
 
+
+
 # %% [markdown]
 # ### 3. Tokenizar un texto utilizando la librería NLTK.
+
+
 
 # %%
 import nltk
@@ -207,27 +281,43 @@ words = [word for word in text.split() if word not in common_words]
 print(' '.join(words))
 
 
+
+
 # %% [markdown]
 # ## Procesamiento de datos de texto
+
+
 
 # %% [markdown]
 # ### 1. Tokenización de texto con NLTK
 
+
+
 # %% [markdown]
 # #### a. Instalar la librería NLTK (Natural Language Toolkit)
+
+
 
 # %%
 !pip install nltk
 
+
+
 # %% [markdown]
 # #### b. Importar NLTK y descargar los recursos necesarios
+
+
 
 # %%
 import nltk
 nltk.download('punkt')
 
+
+
 # %% [markdown]
 # #### c. Tokenizar una oración utilizando la función word_tokenize de NLTK
+
+
 
 # %%
 from nltk.tokenize import word_tokenize
@@ -236,17 +326,27 @@ text = "El gato está en la alfombra."
 tokens = word_tokenize(text)
 print(tokens)
 
+
+
 # %% [markdown]
 # ### 2. Análisis de sentimientos con TextBlob
+
+
 
 # %% [markdown]
 # #### a. Instalar la librería TextBlob
 
+
+
 # %%
 !pip install textblob
 
+
+
 # %% [markdown]
 # #### b. Importar TextBlob y analizar el sentimiento de una oración
+
+
 
 # %%
 from textblob import TextBlob
@@ -256,20 +356,32 @@ blob = TextBlob(text)
 sentiment = blob.sentiment.polarity
 print(sentiment)
 
+
+
 # %% [markdown]
 # ## Procesamiento de datos de imágenes
+
+
 
 # %% [markdown]
 # ### 1. Extracción de características con OpenCV
 
+
+
 # %% [markdown]
 # #### a. Instalar la librería OpenCV
+
+
 
 # %%
 !pip install opencv-python
 
+
+
 # %% [markdown]
 # #### b. Importar OpenCV y cargar una imagen
+
+
 
 # %%
 import cv2
@@ -277,32 +389,50 @@ import matplotlib.pyplot as plt
 
 img = cv2.imread('ruta_de_la_imagen.jpg')
 
+
+
 # %% [markdown]
 # #### c. Convertir la imagen a escala de grises y mostrarla
+
+
 
 # %%
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 plt.imshow(gray_img, cmap='gray')
 
+
+
 # %% [markdown]
 # ### 2. Redes neuronales convolucionales con TensorFlow
+
+
 
 # %% [markdown]
 # #### a. Instalar TensorFlow
 
+
+
 # %%
 !pip install tensorflow
 
+
+
 # %% [markdown]
 # #### b. Importar TensorFlow y cargar un modelo pre-entrenado
+
+
 
 # %%
 import tensorflow as tf
 
 model = tf.keras.applications.MobileNetV2()
 
+
+
 # %% [markdown]
 # #### c. Cargar una imagen y pasarla por el modelo
+
+
 
 # %%
 from tensorflow.keras.preprocessing import image
@@ -315,8 +445,12 @@ img_array = np.expand_dims(img_array, axis=0)
 img_array = tf.keras.applications.mobilenet_v2.preprocess_input(img_array)
 predictions = model.predict(img_array)
 
+
+
 # %% [markdown]
 # #### d. Obtener las clases y las probabilidades de las predicciones
+
+
 
 # %%
 from tensorflow.keras.applications import imagenet_utils
@@ -326,21 +460,33 @@ for result in results[0]:
     print(result)
 
 
+
+
 # %% [markdown]
 # ## Introducción a los datos no estructurados
+
+
 
 # %% [markdown]
 # ### 1. Tokenización de texto
 
+
+
 # %% [markdown]
 # #### a. Importar NLTK y descargar los datos necesarios:
+
+
 
 # %%
 import nltk
 nltk.download('punkt')
 
+
+
 # %% [markdown]
 # #### b. Tokenizar una oración en palabras individuales:
+
+
 
 # %%
 from nltk.tokenize import word_tokenize
@@ -349,11 +495,17 @@ sentence = "Hola a todos, ¿cómo están?"
 tokens = word_tokenize(sentence)
 print(tokens)
 
+
+
 # %% [markdown]
 # ### 2. Análisis de sentimientos
 
+
+
 # %% [markdown]
 # #### a. Importar TextBlob y analizar el sentimiento de una oración:
+
+
 
 # %%
 from textblob import TextBlob
@@ -363,8 +515,12 @@ blob = TextBlob(sentence)
 sentiment = blob.sentiment.polarity
 print(sentiment)
 
+
+
 # %% [markdown]
 # #### b. Analizar el sentimiento de un conjunto de comentarios en una lista:
+
+
 
 # %%
 comments = ['Estoy muy feliz hoy', 'Odio estar enfermo', 'Este restaurante es increíble']
@@ -375,19 +531,29 @@ for comment in comments:
     sentiment_scores.append(sentiment)
 print(sentiment_scores)
 
+
+
 # %% [markdown]
 # ### 3. Extracción de características de imágenes
 
+
+
 # %% [markdown]
 # #### a. Importar OpenCV y cargar una imagen:
+
+
 
 # %%
 import cv2
 
 image = cv2.imread('ruta_de_la_imagen.jpg')
 
+
+
 # %% [markdown]
 # #### b. Convertir la imagen a escala de grises y mostrarla:
+
+
 
 # %%
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -395,11 +561,17 @@ cv2.imshow('Imagen en escala de grises', gray_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+
+
 # %% [markdown]
 # ### 4. Redes neuronales convolucionales para clasificación de imágenes
 
+
+
 # %% [markdown]
 # #### a. Importar TensorFlow y Keras y cargar el conjunto de datos MNIST:
+
+
 
 # %%
 import tensorflow as tf
@@ -408,8 +580,12 @@ from tensorflow import keras
 mnist = keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+
+
 # %% [markdown]
 # #### b. Preprocesar los datos y construir el modelo de red neuronal convolucional:
+
+
 
 # %%
 x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
@@ -428,8 +604,12 @@ model = keras.Sequential([
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
+
+
 # %% [markdown]
 # #### c. Entrenar y evaluar el modelo:
+
+
 
 # %%
 model.fit(x_train, y_train, epochs=5)
